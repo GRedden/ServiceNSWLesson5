7c245db950af517943ffab2cb2259386491d2a68
Glen Redden <glen.redden@studytafense.edu.au>
2020-05-19 21:32:14 +1000

    Comitt

---
+++ Lesson7/DailyChallengeExt.js
@@ -0,0 +1,10 @@
+// Children/Teacher overide or generate random grade.
+
+getInfo()  
+       return `${super.getInfo()} and their average grade is ${this.getAverageGrade()}`
+{
+let myStudent = new Student("Maxwell", "Smart", 25, [87, 68, 78, 91]);
+console.log(myStudent);
+
+console.log(myStudent.getAverageGrade());
+console.log(myStudent.getInfo())};
